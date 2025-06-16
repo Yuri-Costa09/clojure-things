b4eb2e8a5b5de381c84540ab2310ee513c1db1b9
yuri-costa-flooxai <yurircostawork@gmail.com>
2025-06-16 10:21:31 -0300

    + conditionals

--- conditionals.clj
+++ conditionals.clj
@@ -1,16 +1,27 @@
 ;; predicates
 ;; are functions that verify a condition and returns a boolean
-
 ;; example
 (defn tax-free? 
   [salary]
   (< salary 4000))
 
-(def yuri-salary 3999)
+(def yuri-salary 800)
+(def x 15)
 
 (println (tax-free? yuri-salary))
 
 ;; IF example
-(if (<= yuri-salary 4000)
+(if (tax-free? yuri-salary) ;; functions as parameters
   (println "ISENTO!") ;; TRUE
   (println "NAO ISENTO")) ;; FALSE
+
+;; cond example
+(cond 
+  (< x 15) (println "negativo")
+  (> x 15) (println "POSITIVO")
+  :else (println "CACETADA!"))
+
+(condp = x ;; como um switch 
+  < 15 "negativo"
+  > 15 "positivo"
+  "IGUALLLLLLLLLLLLLLLLL")
